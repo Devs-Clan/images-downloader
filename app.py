acc3c2675e70119ca74d18c4c5b3785b00e4a24d
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2023-10-29 00:45:01 +0300

    fixing shit

--- app.py
+++ app.py
@@ -1,45 +1,47 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from chromedriver import chromeDriverSetup 
+from chrome import ChromeConfig 
 from time import sleep
+from datetime import datetime
 import urllib.request
 import os
 
+GOOGLE_PATH = r'https://www.google.com'
+
+
 def main():
-    
-    while(True):
-        searchTerm = input("Enter the images you want to get (This will be the folder name of the data set): ")
-        try:
-            os.mkdir(f'data-set/{searchTerm}') 
-            break
-        except:
-            print("File already exists. Try different name.")  
+    search_term = input("Object to download: ")
+    if search_term not in os.listdir(os.path.join(os.getcwd(), 'data-set')):
+        print(f'Creating folder with the name: {search_term}')
+        os.mkdir(f'data-set/{search_term}')
+    else:
+        print(f'NOTE: new data will added to exsisting directory named: {search_term}')
 
-    options = webdriver.ChromeOptions()        
-    chromeDriverSetup(options)
-    driver = webdriver.Chrome(executable_path="chromedriver.exe", options=options)
-    driver = webdriver.Chrome()
+    options = ChromeConfig(webdriver.ChromeOptions())
+    driver = webdriver.Chrome(options=options)
    
-    searchInGoogle(driver=driver, searchTerm=searchTerm)
+    download_from_google(driver=driver, search_term=search_term)
 
-    sleep(10)
 
-def searchInGoogle(driver, searchTerm):
-    driver.get('https://www.google.com')
+def download_from_google(driver, search_term):
+    driver.get(GOOGLE_PATH)
     driver.maximize_window()
     
     input = driver.find_element_by_xpath("//input[@name='q']")
     input.clear()
-    input.send_keys(searchTerm)
+    input.send_keys(search_term)
     input.send_keys(Keys.ENTER)
-    sleep(3)
+    sleep(2)
     tabs = driver.find_element_by_css_selector('div.MUFPAc>:nth-child(2)>:first-child')
     tabs.send_keys(Keys.ENTER)
 
-    for i in range (1, 10):
+    for i in range (1, 200):
+        if i % 25 == 0:
+            continue
         image = driver.find_element_by_css_selector(f"div.islrc>:nth-child({i})>:first-child>:first-child>:first-child")
         src = image.get_attribute('src')
-        urllib.request.urlretrieve(src, f"data-set/{searchTerm}/image{i}.jpg")
+        current_datetime = datetime.now().strftime(f'%d-%m-%Y, %H:%M:%S')
+        urllib.request.urlretrieve(src, f"data-set/{search_term}/{current_datetime}.jpg")
 
-main()  
+if __name__ == "__main__":
+    main()  
--- chrome.py
+++ chrome.py
@@ -1,5 +1,5 @@
 
-def chromeDriverSetup(options):
+def ChromeConfig(options):
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
     options.headless = True
     options.add_argument(f'user-agent={user_agent}')
@@ -12,5 +12,6 @@ def chromeDriverSetup(options):
     options.add_argument("--start-maximized")
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--no-sandbox')    
+    options.add_argument('--no-sandbox')
+    return options
         
